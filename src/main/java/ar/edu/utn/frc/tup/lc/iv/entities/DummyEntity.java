8808625d2283a1cd49a2aad6499902e1976063c1
405193-OLIVA <164232458+405193-OLIVA@users.noreply.github.com>
2024-09-23 21:13:40 -0300

    Initial commit

---
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/entities/DummyEntity.java
@@ -0,0 +1,14 @@
+package ar.edu.utn.frc.tup.lc.iv.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class DummyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String dummy;
+}
---
+++ src/test/java/ar/edu/utn/frc/tup/lc/iv/AppTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frc.tup.lc.iv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
